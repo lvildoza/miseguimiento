c19ecdcc89ac26ed66b804a5fe3b9af6e9bb6f40
JDnasello <jnasellopron@gmail.com>
2024-07-22 15:31:14 -0300

    Feat: Added table for seguimientos

---
+++ miseguimiento-front/src/pages/Seguimientos.jsx
@@ -0,0 +1,11 @@
+import TableSeguimientos from "../components/TableSeguimientos.jsx"
+
+const Seguimientos = () => {
+    return (
+        <div>
+            <TableSeguimientos />
+        </div>
+    )
+}
+
+export default Seguimientos
