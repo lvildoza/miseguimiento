ffbca97c6f4b7c58c808d2e5505595e89f8c1200
Leandro M. Vildoza <leandro@losinformaticos.com.ar>
2024-07-24 17:04:12 -0300

    Fix: delete frontend

--- miseguimiento-front/src/context/Context.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-// Creación de contexto para Seguimiento y Status 
-export const SeguimientoContext = createContext()
-export const StatusContext = createContext()
